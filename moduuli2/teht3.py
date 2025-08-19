975358bab7550c8969829ef8f7d6e009930a924e
eetu <eetu.vaananen@metropolia.fi>
2025-08-19 19:42:38 +0300

    update

---
+++ moduuli2/teht3.py
@@ -0,0 +1,8 @@
+kanta = input("Kerro kanta: ")
+korkeus = input("Kerro korkeus: ")
+
+piiri = 2 * float(kanta) + 2 * float(korkeus)
+pinta = float(kanta) * float(korkeus)
+
+print(f"Piiri: {piiri:.2f}")
+print(f"Pinta-ala: {pinta:.2f}")
