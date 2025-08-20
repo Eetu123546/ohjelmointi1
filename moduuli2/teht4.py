329b61a2cf52b256983668b5ca88504c882ac814
eetu <eetu.vaananen@metropolia.fi>
2025-08-20 19:35:28 +0300

    update

--- moduuli2/teht4.py
+++ moduuli2/teht4.py
@@ -4,4 +4,10 @@ num1 = int(input("Numero yksi: "))
 num2 = int(input("Numero kaksi: "))
 num3 = int(input("Numero kolme: "))
 
+summa = num1 + num2 + num3
+tulo = num1 * num2 * num3
+keskiarvo = summa / 3
 
+print(f"Summa: {summa:.2}")
+print(f"Tulo: {tulo:.2}")
+print(f"Keskiarvo: {keskiarvo:.2}")
