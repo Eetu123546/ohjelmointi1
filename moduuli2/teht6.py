3ac561d1536b9e2bae737f93c62d2c0eec5fe87f
eetu <eetu.vaananen@metropolia.fi>
2025-08-20 22:14:47 +0300

    update

--- moduuli2/teht6.py
+++ moduuli2/teht6.py
@@ -1,4 +1,13 @@
 import random
 
-test = random.randint(2,5)
-print(test)
+kolme1 = random.randint(0,9)
+kolme2 = random.randint(0, 9)
+kolme3 = random.randint(0, 9 )
+
+nelja1 = random.randint(1, 6 )
+nelja2 = random.randint(1, 6)
+nelja3 = random.randint(1, 6)
+nelja4 = random.randint(1, 6 )
+
+print(f"Ensimmäinen koodi: {kolme1} {kolme2} {kolme3}")
+print(f"Toinen koodi: {nelja1} {nelja2} {nelja3} {nelja4}")
