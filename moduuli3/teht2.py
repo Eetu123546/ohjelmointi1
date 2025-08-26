99dbcf36c4fbd300926193858490e04817f73bc3
eetu <eetu.vaananen@metropolia.fi>
2025-08-26 14:04:23 +0300

    update

---
+++ moduuli3/teht2.py
@@ -0,0 +1,16 @@
+hyt = input("Anna hyttiluokka: ")
+
+if hyt == "LUX":
+    print("LUX on parvekkeellinen hytti yläkannella.")
+
+elif hyt == "A":
+    print("A on ikkunallinen hytti autokannen yläpuolella.")
+
+elif hyt == "B":
+    print("B on ikkunaton hytti autokannen yläpuolella.")
+
+elif hyt == "C":
+    print("C on ikkunaton hytti autokannen alapuolella.")
+
+else:
+    print("Virheellinen hyttiluokka!")
