cef54b07153857aef9f79bd6c8c0e675bb2fe21a
eetu <eetu.vaananen@metropolia.fi>
2025-08-26 14:32:51 +0300

    update

---
+++ moduuli3/teht3.py
@@ -0,0 +1,26 @@
+suku = input("Kerro sukupuoli: ")
+hemo = float(input("Kerro hemoglobiini(g/l): "))
+
+if suku == "mies":
+    if hemo < 134:
+        print("liian alhainen hemoglobiini")
+    elif hemo > 195:
+        print("liian korkea hemoglobiini")
+    else:
+        print("normaali hemoglobiini")
+
+elif suku == "nainen":
+    if hemo < 117:
+        print("liian alhainen hemoglobiini")
+    elif hemo > 175:
+        print("liian korkea hemoglobiini")
+    else:
+        print("normaali hemoglobiini")
+
+else:
+    print("sukupuoli annettu väärin")
+
+
+
+
+print("testi")
