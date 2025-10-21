b2562f5f8c5876a556cb22e908292427c63979aa
eetu <eetu.vaananen@metropolia.fi>
2025-10-21 15:31:29 +0300

    update

--- moduuli9/teht2.py
+++ moduuli9/teht2.py
@@ -6,8 +6,8 @@ auto1.kiihdytä(30)
 auto1.kiihdytä(70)
 auto1.kiihdytä(50)
 
-print(f"Auton nopeus: {auto1.nopeus}")
+print(f"Auton nopeus: {auto1.nopeus} km/h.")
 
 auto1.kiihdytä(-200)
 
-print(f"Auton nopeus jarrutuksen jälkeen: {auto1.nopeus}")
+print(f"Auton nopeus jarrutuksen jälkeen: {auto1.nopeus} km/h.")
